528ee6d87aba8fd7ef999030ea7e66e071b0200f
Yusuf <93922731+Zuf0831@users.noreply.github.com>
2021-11-08 20:35:29 +0700

    Add files via upload

---
+++ Project/index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import Naviga from './Components/Navigation';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => Naviga);
